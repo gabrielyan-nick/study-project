15be7be1a756d9b4664076d187f9895b0dcb0853
Nick <junk72305@gmail.com>
2022-08-20 20:18:20 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+            tabsParent = document.querySelector('.tabheader__items'),
+            tabsBtn = document.querySelectorAll('.tabheader__item');
+
+    
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade'); 
+            item.classList.add('hide'); 
+        });
+
+        tabsBtn.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+ 
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabsBtn[i].classList.add('tabheader__item_active');
+    }
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabsBtn.forEach((item, i) => {
+                if (item == target) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+hideTabContent();
+showTabContent();
+}); 
+
+
+
