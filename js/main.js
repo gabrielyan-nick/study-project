02d09ed220d311faaec812254977b1fe07dd584c
Nick <junk72305@gmail.com>
2022-08-22 20:56:22 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    //tabs
+
     const tabsContent = document.querySelectorAll('.tabcontent'),
             tabsParent = document.querySelector('.tabheader__items'),
             tabsBtn = document.querySelectorAll('.tabheader__item');
@@ -39,6 +41,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
 hideTabContent();
 showTabContent();
+
+//timer
+
+const deadline = '2022-08-29';
+
+function getTimeRemaining(endtime) {   // определяем оставшееся время, результат в обьект
+    const t = Date.parse(endtime) - Date.now(),              
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+          minutes = Math.floor(t / (1000 * 60) % 60),
+          seconds = Math.floor((t / 1000) % 60);
+                                                              
+    return {
+        t,
+        days,
+        hours,
+        minutes,
+        seconds
+    }; 
+}
+
+function getZero(num) {          // добавляем 0 к цифре
+    if (num < 10 && num >= 0) {
+       return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) { 
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          min = timer.querySelector('#minutes'),
+          sec = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000); // обновляем таймер каждую сек
+
+    updateClock();
+
+    function updateClock() {                    // записываем результат в верстку 
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);   // добавляем 0 к дню
+        hours.innerHTML = getZero(t.hours);
+        min.innerHTML = getZero(t.minutes);
+        sec.innerHTML = getZero(t.seconds);
+       
+        if (t.total <= 0) {
+            clearInterval(timeInterval);        // выкл обновление, если время истекло
+        }
+    }
+
+}
+
+setClock('.timer', deadline);
+
+
+
+
+
+
+
+
+
 }); 
 
 
